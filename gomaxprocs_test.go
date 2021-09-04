64942da61067c7130d506ac84a9f3ea00320a5b2
vandy ahmad <vandyahmad2404@gmail.com>
2021-09-05 05:18:28 +0700

    gomaxprocs

---
+++ gomaxprocs_test.go
@@ -0,0 +1,29 @@
+package go_routine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomaxproc(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i:=0; i<100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3*time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("Total CPU",totalCpu)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Total thread",totalThread)
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Total goroutine",totalGoroutine)
+	group.Wait()
+
+}
